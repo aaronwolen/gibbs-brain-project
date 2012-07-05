a13c964eb44c389556f5fd5f38eba6c32ae42fe3
Aaron Wolen <aaron@wolen.com>
2012-07-05 17:41:47 -0400

    Use new public gist address

--- functions/load_gists.r
+++ functions/load_gists.r
@@ -1,4 +1,4 @@
 library(devtools)
 
 # dataframetools
-source_gist("https://raw.github.com/gist/540b67c86c3c81220462/785b87f8817cff500cd9d5be1c51a082db1cc842/dataframetools.r")
+source_gist("3056626")
