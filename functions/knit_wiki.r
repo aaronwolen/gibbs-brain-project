7a0e1ddd1d39f27b617dea775a03821834ab00df
Aaron Wolen <aaron@wolen.com>
2012-07-15 17:55:30 -0400

    Initial commit

---
+++ functions/knit_wiki.r
@@ -0,0 +1,24 @@
+
+#' @param filename input filename
+#' @param output.path output file path
+#' @param output file type 
+#' @param output.filename (optional) different basename for output file. This
+#'   may also be a function that manipulates the input filename
+
+knit_wiki <- function(filename, output.path = "wiki", output.ext = "md",
+  output.filename,
+  git.path = "https://github.com/aaronwolen/gibbs-brain-project/blob/master/") {
+  require(knitr, quietly = TRUE)
+  
+  # Create output filename and add proper extension 
+  output <- ifelse(missing(output.filename), filename, output.filename)
+  output <- gsub("\\.\\w+$|$", paste("\\.", output.ext, sep = ""), output)
+  
+  # knit document and save output in wiki directory
+  output <- file.path(output.path, output)
+  
+  
+  knit(filename, output)
+  
+  # Point all image links to github url
+}
