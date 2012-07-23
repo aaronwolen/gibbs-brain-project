259fbeb91c0643ee814bda8adc3e6d3db5abf885
Aaron Wolen <aaron@wolen.com>
2012-07-23 12:19:56 -0400

    Correct load-gists filename

--- 2-reformat-data.r
+++ 2-reformat-data.r
@@ -8,7 +8,7 @@ library(Biobase)
 library(plyr)
 library(ggplot2)
 source("functions/cleaning-functions.r")
-source("functions/load_gists.r")
+source("functions/load-gists.r")
 
 # Data' -------------------------------------------------------------------
 load("data/GSE15745_raw-data.rda")
