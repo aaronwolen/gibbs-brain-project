fb7287c26670f432e007848da1627c269296b9c2
Aaron Wolen <aaron@wolen.com>
2012-07-31 11:31:53 -0400

    Remove samples based on sample median deviation analysis added in SHA: 35031b6

--- 4b-load-meth-data.r
+++ 4b-load-meth-data.r
@@ -7,6 +7,18 @@ load("data/eset-meth.rda")
 
 # Filter samples based on qc analysis -------------------------------------
 
+# Remove samples squared median deviations 2 SD's above the mean
+ref.comps <- read.csv("figures/qc-methylation/meth-median-ref-comparisons.csv",
+  stringsAsFactors = FALSE)
+
+outlier <- sampleNames(eset.meth) %in% ref.comps$sample[ref.comps$outlier]
+cat("Removed", sum(outlier),
+    "samples deemed outliers based on their deviations from the tissue-median.\n")
+
+eset.meth <- eset.meth[, !outlier]
+rm(list = c("ref.comps", "outlier"))
+
+
 # Remove samples with an excessive number of missing probes.
 # (meth-qc_NA-probes-histogram.pdf)
 
@@ -19,6 +31,8 @@ cat("Removed", sum(!na.filter),
 eset.meth <- eset.meth[, na.filter]
 rm(na.filter)                    
 
+
+# Sample filters ----------------------------------------------------------
 
 # Remove outlier samples with median intensities > 0.2
 # (meth-qc_histogram.pdf, meth-qc_boxplot.pdf)
