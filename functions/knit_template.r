bc0c30f8d4bbeea0b299339454ae65c9f598c824
Aaron Wolen <aaron@wolen.com>
2012-09-04 14:30:56 -0400

    Initial commit of knit_template

---
+++ functions/knit_template.r
@@ -0,0 +1,29 @@
+#' Use brew to programmatically set knitr chunk labels 
+
+#' @param file a character string naming the file to be read in. 
+#' @param label character string matching text within brew delimiters
+#' @param labels character vector containing values to assign to label
+
+knit_template <- function(file, variable = "variable", labels) {
+  require(knitr, quietly = TRUE)
+
+  # Set knitr to read brew syntax
+  pat_brew()
+  knit_hooks$set(inline = identity)
+  
+  src <- NULL
+    
+  # Process template file for each level of supplied labels
+  for(i in labels) {
+    assign(variable, i)
+    src <- c(src, knit(text = readLines(file)))
+  }
+  
+  # Switch back to markdown syntax
+  pat_md()
+  render_markdown()
+  
+  out <- knit(text = src)
+  
+  return(out)
+}
