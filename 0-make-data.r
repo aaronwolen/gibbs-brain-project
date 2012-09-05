86527b5f13d02434c9905f33a9ac643e86f44125
Aaron Wolen <aaron@wolen.com>
2012-09-05 12:02:50 -0400

    Verify checksums and remove raw data

--- 0-make-data.r
+++ 0-make-data.r
@@ -12,10 +12,26 @@ if(length(package.paths) < length(packages)) {
   stop("\n\nInstall the packages listed above before proceeding", call. = FALSE)
 }
 
+
 # Download data -----------------------------------------------------------
 source("1-download-data.r")
 
 
 # Clean data and export rda files -----------------------------------------
 source("2-reformat-data.r")
+
+
+# Check integrity of rda files --------------------------------------------
+exp.verified <- devtools:::md5("data/eset-exp.rda") == "3f9afdefd2469691f9d54f916d33089a"
+meth.verified <- devtools:::md5("data/eset-meth.rda") == "800538004a90b6e3b9c11b3497fc7db9"
+micro.verified <- devtools:::md5("data/eset-micro.rda") == "604b2f6dcb5de62cd80e4bdd8c5fc658"
+
+
+# Remove raw geo data -----------------------------------------------------
+if(all(exp.verified, meth.verified, micro.verified)) {
+  file.remove(c("data/geo-raw-data.rda", dir("data/geo", full.names = TRUE)))  
+} else {
+  stop("rda files generated from GEO downloads differ from Aaron's.", call. = FALSE)
+}
+
 
